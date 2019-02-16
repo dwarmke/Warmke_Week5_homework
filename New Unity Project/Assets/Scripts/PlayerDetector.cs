dfebe20d3d3e1d07075a4448c00c797d4dd6aed4
Daniel Warmke <dwarmke@yahoo.com>
2019-02-16 11:54:26 -0500

    GameUpdated

---
+++ New Unity Project/Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10))
+        {
+            print(hit.collider.gameObject.name);
+        }
+        else
+        {
+        print("NOTHING AHEAD");
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform targetPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent> (); 
+        agent.destination = targetPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
