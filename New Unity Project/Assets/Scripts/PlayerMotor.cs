c5572c1f1dde8df23fde2e30e4edfd5739d577f7
Daniel Warmke <dwarmke@yahoo.com>
2019-02-17 07:03:52 -0500

    final

--- New Unity Project/Assets/Scripts/PlayerMotor.cs
+++ New Unity Project/Assets/Scripts/PlayerMotor.cs
@@ -6,17 +6,21 @@ using UnityEngine.AI;
 public class PlayerMotor : MonoBehaviour
 {
     private NavMeshAgent agent;
+    //private Rigidbody myRigidbody; 
+    //I built capabuility to do an update movment like the promp said but switched to Nav mesh for cooler results. 
+    //You can remove the comment sleshes and comment out the agent destination to use the other sistem. 
     public Transform targetPoint;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent> (); 
         agent.destination = targetPoint.position;
+        //myRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //myRigidbody.velocity = transform.forward * .5f;
     }
 }
